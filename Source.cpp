199bd3af977c5c06e931ae91ae8cbd312de54659
ash <32853027+asaah18@users.noreply.github.com>
2019-02-02 16:59:55 +0300

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int Base_b_expansion(int number, int base); //base > 1	|	number & base are positve(+)
+
+int Add(int a, int b);	//a & b are positve(+)
+
+int Modular_exponentiation(int b, /*n = (ak-1ak-2...a1a0)2,*/ int m/*positive(+)*/);
+
+int Multiply(int a, int b);	//a & b are positive(+)
+
+//int function();
+
+int main() 
+{
+	//test here
+	return 0;
+}	 
