eb03fb93335b289fcb3667ab0a2ae68a2c5ea468
Abdullah Alkhulifi <38915416+ABK9@users.noreply.github.com>
2019-04-09 20:15:34 +0300

    Modular_exponentiation() been added .

--- Source.cpp
+++ Source.cpp
@@ -6,7 +6,22 @@ int Base_b_expansion(int number, int base); //base > 1	|	number & base are posit
 
 int Add(int a, int b);	//a & b are positve(+)
 
-int Modular_exponentiation(int b, /*n = (ak-1ak-2...a1a0)2,*/ int m/*positive(+)*/);
+int Modular_exponentiation(int b, /*n = (ak-1ak-2...a1a0)2,*/ int m/*positive(+)*/){
+
+int b, m,i,power,k,a;
+int x = 1 ;
+
+power = b % m ;
+
+for (i=0;i<=k;i++){
+
+  if (a = 1) {
+     x= x*power;
+     x = x % m;
+     power= (power*power)% m ;
+
+}
+
 
 int Multiply(int a, int b);	//a & b are positive(+)
 
