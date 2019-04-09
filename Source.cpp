e41267282f9d40a14759913be10f782aae50b1a3
ash <32853027+asaah18@users.noreply.github.com>
2019-04-09 22:10:14 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,24 +2,24 @@
 #include <string>
 using namespace std;
 
-int Base_b_expansion(int number, int base); //base > 1	|	number & base are positve(+)
+// int Base_b_expansion(int number, int base); //base > 1	|	number & base are positve(+)	|	done individually
 
 int Add(int a, int b);	//a & b are positve(+)
 
-int Modular_exponentiation(int b, /*n = (ak-1ak-2...a1a0)2,*/ int m/*positive(+)*/){
+int Modular_exponentiation(int b, /*n = (ak-1ak-2...a1a0)2,*/ int m/*positive(+)*/)
+{
+	int b, m,i,power,k,a;
+	int x = 1 ;
 
-int b, m,i,power,k,a;
-int x = 1 ;
+	power = b % m ;
 
-power = b % m ;
-
-for (i=0;i<=k;i++){
-
-  if (a = 1) {
-     x= x*power;
-     x = x % m;
-     power= (power*power)% m ;
-
+	for (i=0;i<=k;i++){
+	  if (a = 1) {
+	     x= x*power;
+	     x = x % m;
+	     power= (power*power)% m ;
+		  
+	return power;
 }
 
 
